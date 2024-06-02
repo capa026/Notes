9833a4b46fc4e8d5df3b519e8dc4668887d32270
capa026 <carlospacheco.fx@gmail.com>
2024-06-02 11:02:12 -0400

    Server api link fixed in .env file

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_SERVER,
+  baseURL: import.meta.env.VITE_API_SERVER + "/api",
   withCredentials: true,
 });
 

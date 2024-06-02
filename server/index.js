20f2957b06c04ba732b8617f31ff3196689ed10f
capa026 <carlospacheco.fx@gmail.com>
2024-06-02 11:09:03 -0400

    Updated cors origin allowed in cors middleware

--- server/index.js
+++ server/index.js
@@ -10,10 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://carlospacheconotes.netlify.app/",
-    ],
+    origin: "*",
     credentials: true,
   })
 );
